135074b6d13e95b51ce60dfef7295fc4ac867a8d
Hahnsang Kim <hahnsang@Hahnsangs-MacBook-Pro.local>
2019-01-21 21:21:29 -0800

    add rinkeby in truffle

--- truffle.js
+++ truffle.js
@@ -1,5 +1,6 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+const HDWalletProvider = require("truffle-hdwallet-provider");
 
 // Edit truffle.config file should have settings to deploy the contract to the Rinkeby Public Network.
 // Infura should be used in the truffle.config file for deployment to Rinkeby.
@@ -10,6 +11,19 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
-    }
+    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 9545,
+    //   network_id: "*" // Match any network id
+    // },
+    rinkeby: {
+      provider: function() {
+        // return new HDWalletProvider("<12 words mnemonic>", "4dfinfura address")
+        return new HDWalletProvider("<12 words mnemonic>", "4dfinfura address")      },
+          network_id: '4',
+          gas: 4500000,
+          gasPrice: 10000000000,
+    }    
   }
 }
