080a0796792cf4078a5afca503149c954430bfb6
H. Kim <fido2478@users.noreply.github.com>
2019-01-22 20:36:35 -0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -20,7 +20,7 @@ module.exports = {
     rinkeby: {
       provider: function() {
         // return new HDWalletProvider("<12 words mnemonic>", "4dfinfura address")
-        return new HDWalletProvider("place urge proud despair aisle sound recall humor domain assume eternal wealth", "https://rinkeby.infura.io/v3/1f82a86b4dfd4f70a05f352c81ba232d")
+        return new HDWalletProvider("12 words mnemonic", "https://rinkeby.infura.io/v3/1f82a86b4dfd4f70a05f352c81ba232d")
       },
           network_id: '4',
           gas: 4500000,
